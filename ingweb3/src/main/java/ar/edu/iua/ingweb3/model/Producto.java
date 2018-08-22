bfb9c35123c5d13b1856c828d6a3878f040272b0
magm3333 <magm3333@gmail.com>
2018-08-22 11:30:11 -0300

    primer servicio rest

---
+++ ingweb3/src/main/java/ar/edu/iua/ingweb3/model/Producto.java
@@ -0,0 +1,66 @@
+package ar.edu.iua.ingweb3.model;
+
+import java.util.Date;
+
+public class Producto {
+
+	public Producto() {
+	}
+
+	public Producto(int id, String descripcion, double precio, boolean enStock, Date vencimiento) {
+		super();
+		this.id = id;
+		this.descripcion = descripcion;
+		this.precio = precio;
+		this.enStock = enStock;
+		this.vencimiento = vencimiento;
+	}
+
+	private int id;
+
+	private String descripcion;
+	private double precio;
+	private boolean enStock;
+	private Date vencimiento;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public boolean isEnStock() {
+		return enStock;
+	}
+
+	public void setEnStock(boolean enStock) {
+		this.enStock = enStock;
+	}
+
+	public Date getVencimiento() {
+		return vencimiento;
+	}
+
+	public void setVencimiento(Date vencimiento) {
+		this.vencimiento = vencimiento;
+	}
+
+}
