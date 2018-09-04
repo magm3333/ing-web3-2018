a6c7dba075b2df199fec389b0bbbf35755ca7295
magm3333 <magm3333@gmail.com>
2018-09-04 18:51:23 -0300

    agregamos lógica de negocios

---
+++ ingweb3/src/main/java/ar/edu/iua/ingweb3/model/exception/NotFoundException.java
@@ -0,0 +1,26 @@
+package ar.edu.iua.ingweb3.model.exception;
+
+public class NotFoundException extends Exception{
+
+	public NotFoundException() {
+		super();
+	}
+
+	public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+	public NotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public NotFoundException(String message) {
+		super(message);
+	}
+
+	public NotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+
+}
