7b499497b09324a536d3e75c26eda2996e231c62
magm3333 <magm3333@gmail.com>
2018-09-05 19:30:07 -0300

    servicios completos para producto, simulados

--- ingweb3/src/main/java/ar/edu/iua/ingweb3/web/services/ProductosRESTController.java
+++ ingweb3/src/main/java/ar/edu/iua/ingweb3/web/services/ProductosRESTController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.edu.iua.ingweb3.business.BusinessException;
@@ -21,46 +22,75 @@ import ar.edu.iua.ingweb3.model.exception.NotFoundException;
 @RequestMapping("/productos")
 public class ProductosRESTController {
 
-	
-	
 	@Autowired
 	private IProductoBusiness productoBusiness;
-	
+
 	@RequestMapping(value = { "", "/" }, method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<List<Producto>> lista() {
+	public ResponseEntity<List<Producto>> lista(
+			@RequestParam(required = false, value = "q", defaultValue = "*") String q) {
 
 		try {
-			return new ResponseEntity<List<Producto>>(productoBusiness.getAll(),HttpStatus.OK);
+			if (q.equals("*") || q.trim().length() == 0) {
+				return new ResponseEntity<List<Producto>>(productoBusiness.getAll(), HttpStatus.OK);
+			} else {
+				return new ResponseEntity<List<Producto>>(productoBusiness.search(q), HttpStatus.OK);
+			}
 		} catch (BusinessException e) {
 			return new ResponseEntity<List<Producto>>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
-	
-	@RequestMapping(value = {  "/{id}" }, method = RequestMethod.GET, produces = "application/json")
+
+	@RequestMapping(value = { "/{id}" }, method = RequestMethod.GET, produces = "application/json")
 	public ResponseEntity<Producto> uno(@PathVariable("id") int id) {
 		try {
-			return new ResponseEntity<Producto>(productoBusiness.getOne(id),HttpStatus.OK);
+			return new ResponseEntity<Producto>(productoBusiness.getOne(id), HttpStatus.OK);
 		} catch (BusinessException e) {
 			return new ResponseEntity<Producto>(HttpStatus.INTERNAL_SERVER_ERROR);
 		} catch (NotFoundException e) {
 			return new ResponseEntity<Producto>(HttpStatus.NOT_FOUND);
 		}
-		
+
 	}
 
 	@RequestMapping(value = { "", "/" }, method = RequestMethod.POST, produces = "application/json")
 	public ResponseEntity<Object> add(@RequestBody Producto producto) {
-		
 		try {
 			productoBusiness.add(producto);
 			HttpHeaders responseHeaders = new HttpHeaders();
 			responseHeaders.set("location", "/productos/" + producto.getId());
-			return new ResponseEntity<Object>(responseHeaders,HttpStatus.CREATED);
+			return new ResponseEntity<Object>(responseHeaders, HttpStatus.CREATED);
 		} catch (BusinessException e) {
 			return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 	}
 
-	
+	@RequestMapping(value = { "", "/" }, method = RequestMethod.PUT, produces = "application/json")
+	public ResponseEntity<Object> update(@RequestBody Producto producto) {
+		try {
+			productoBusiness.update(producto);
+			return new ResponseEntity<Object>(HttpStatus.OK);
+		} catch (BusinessException e) {
+			return new ResponseEntity<Object>(HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<Object>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+
+	@RequestMapping(value = { "/{id}" }, method = RequestMethod.DELETE, produces = "application/json")
+	public ResponseEntity<Producto> delete(@PathVariable("id") int id) {
+		try {
+			Producto p = new Producto();
+			p.setId(id);
+			productoBusiness.delete(p);
+			return new ResponseEntity<Producto>(HttpStatus.OK);
+		} catch (BusinessException e) {
+			return new ResponseEntity<Producto>(HttpStatus.INTERNAL_SERVER_ERROR);
+		} catch (NotFoundException e) {
+			return new ResponseEntity<Producto>(HttpStatus.NOT_FOUND);
+		}
+
+	}
+
 }
